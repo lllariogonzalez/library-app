58b614fc2ae033644b092cb40659362a36a8aef5
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-25 16:58:22 -0300

    add User model

---
+++ src/main/java/models/Users.java
@@ -0,0 +1,59 @@
+package models;
+
+public class Users {
+    private int id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String address;
+    private String tel;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+}
