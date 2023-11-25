666a9bf7a76dab90cd03f64e014b0c67dc15a75f
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-25 16:56:30 -0300

    add Notification interface

---
+++ src/main/java/interfaces/NotificationInterface.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface NotificationInterface {
+  public void showErrorNotification(String message);
+  public void showSuccessNotification(String message);
+  public void showInformationNotification(String message);
+}
