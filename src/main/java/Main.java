1553b0ddcfe22338c8cdc3475a3abadd9c066f57
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-25 17:11:50 -0300

    add Main java start aplication

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialLighterIJTheme;
+
+import views.Dashboard;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+  public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        FlatMaterialLighterIJTheme.setup();
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Dashboard().setVisible(true);
+            }
+        });
     }
 }
-
