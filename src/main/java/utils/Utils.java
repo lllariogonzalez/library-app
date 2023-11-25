aab55b8bf09d8cbc97447ebf14c774cc681199c0
lllariogonzalez <lllariogonzalez@gmail.com>
2023-11-25 17:00:16 -0300

    add Utils methods for validation and format date

---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,74 @@
+package utils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
+public class Utils {
+    
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            Integer.parseInt(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+    
+    public static String getCurrentDate() {
+        Date now = new Date();
+        SimpleDateFormat formateador = new SimpleDateFormat("dd-MM-yyyy");
+        return formateador.format(now);
+    }
+
+    public static String getCurrentDateComplete(){
+        LocalDate now = LocalDate.now();
+        Locale spanishLocale = new Locale("es", "ES");
+        return now.format(DateTimeFormatter.ofPattern("'Hoy es' EEEE dd 'de' MMMM 'de' yyyy", spanishLocale));
+    }
+    
+    //Diferencias entre dos fechas
+    //@param fechaInicial La fecha de inicio
+    //@param fechaFinal  La fecha de fin
+    //@return Retorna el numero de dias entre dos fechas
+    public static synchronized int getDaysBetweenDates(Date fechaInicial, Date fechaFinal) throws ParseException {
+
+        DateFormat df = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        String fechaInicioString = df.format(fechaInicial);
+        try {
+            fechaInicial = df.parse(fechaInicioString);
+        } catch (ParseException ex) {
+        }
+
+        String fechaFinalString = df.format(fechaFinal);
+        fechaFinal = df.parse(fechaFinalString);
+
+        long fechaInicialMs = fechaInicial.getTime();
+        long fechaFinalMs = fechaFinal.getTime();
+        long diferencia = fechaFinalMs - fechaInicialMs;
+        double dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+        return ((int) dias);
+    }
+
+    //Devuele un java.util.Date desde un String en formato dd-MM-yyyy
+    //@param La fecha a convertir a formato date
+    //@return Retorna la fecha en formato Date
+    public static synchronized Date stringToDate(String fecha) {
+        SimpleDateFormat formatoDelTexto = new SimpleDateFormat("dd-MM-yyyy");
+        Date fechaEnviar = null;
+        try {
+            fechaEnviar = formatoDelTexto.parse(fecha);
+            return fechaEnviar;
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+}
